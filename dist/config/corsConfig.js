c5c0e65417f25bc5ae48682e8c31693349739f67
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-01-22 18:19:31 -0300

    Heroku

--- dist/config/corsConfig.js
+++ dist/config/corsConfig.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptions = void 0;
+require("dotenv/config");
 const corsOptions = {
     origin: [
         "https://next-school.vercel.app",
         "https://next-school-52p4zsoke-jrsmarcilio.vercel.app",
+        "https://school-ads.herokuapp.com",
     ],
     credentials: true,
     optionsSuccessStatus: 200,
--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,9 +1,11 @@
+import "dotenv/config";
 import { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
   origin: [
     "https://next-school.vercel.app",
     "https://next-school-52p4zsoke-jrsmarcilio.vercel.app",
+    "https://school-ads.herokuapp.com",
   ],
   credentials: true,
   optionsSuccessStatus: 200,
