1eb8aaf5f2cf16b7979fdd8a87a21a95d6ef8c20
Marcilio Júnior <jrsmarcilio@gmail.com>
2022-01-21 23:38:59 -0300

    add heroku configs vars

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import { createConnection } from "typeorm";
-
+console.log(process.env.CLEARDB_DATABASE_URL);
 createConnection({
   type: "mysql",
   url: process.env.CLEARDB_DATABASE_URL,
   entities: [__dirname + "/../entities/*.ts"],
   synchronize: true,
-  ssl: true,
 })
   .then(() => {
     console.log("Connected");
